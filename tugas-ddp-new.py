e09ad6f7da9ba43f28f4871f84cfee6dac87f422
fatahfkrn <92284251+fatahfkrn@users.noreply.github.com>
2021-11-03 21:02:09 +0700

    Add files via upload

--- tugas-ddp-new.py
+++ tugas-ddp-new.py
@@ -1,6 +1,6 @@
-# Nama : Fatah Muhammad Fikrudin
-# NIM : 0110121162
-# Kelas : SI08
+# # Nama : Fatah Muhammad Fikrudin
+# # NIM : 0110121162
+# # Kelas : SI08
 
 print("\n\n")
 print("Fitur Belanja")
@@ -18,48 +18,91 @@ while a != "X":
     print()
     i += 1
     j += b
+    setelah_diskon = j
+    print()
+    print("Total produk yang dibeli: ", i)
+    if i == 0:
+        print
+    print("Total harga produk: ", float(j))
 
-print()
-print("Total produk yang dibeli: ", i)
-if i == 0:
-    print
-print("Total harga produk: ", float(j))
+    print("\n")
+    anggota = str(input("Apakah anda seorang anggota? (Y/T): "))
+    if anggota == "Y" or anggota == "y":
+        def check_email(string):
+            if ' ' in string:
+                return False
+            if "@" not in string:
+                return False
+            name, domain = string.split('@', 1)
+            if '.' not in domain[1:]:
+                return False
+            return True
+        email = input("Masukkan email: ")
+        while True:
+            if check_email(email):
+                print("Email valid")
+                break
+            else:
+                email = input("Email tidak valid, ulangi: ")
 
-print("\n")
-anggota = str(input("Apakah anda seorang anggota? (Y/T): "))
-if anggota == "Y" or anggota == "y":
-    email = input("Masukkan Email: ")
-    while email != "Valid":
-        email = input("Email tidak Valid, ulangi: ")
-    pw = input("Masukkan Password!: ")
-    while pw != "valid":
-        pw = input("Password tidak valid, ulangi!: ")
-    level = input("Masukkan Level Kepesertaan Anda (Silver/Gold/Diamond): ")
-    while level != "Silver" and level != "Gold" and level != "Diamond":
-        level = input ("Masukkan tidak valid, ulangi: ")
-        if level == "Silver":
-            print()
-            if i < 5:
-                print("Selamat! anda mendapatkan potongan harga sebesar 5%")
+        # def check_pw(string)
+
+        level = input("Masukkan Level Kepesertaan Anda (Silver/Gold/Diamond): ")
+        while level != "Silver" and level != "Gold" and level != "Diamond":
+            level = input ("Masukkan tidak valid, ulangi: ")
+        else:
+            if level == "Silver":
+                if i < 5:
+                    diskon = j * (5/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 5%")
+                else:
+                    diskon = j * (10/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 10%")
+            elif level == "Gold":
+                if i < 5:
+                    diskon = j * (10/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 10%")
+                else:
+                    diskon = j * (15/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 15%")
+            elif level == "Diamond":
+                if i < 5:
+                    diskon = j * (15/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 15%")
+                else:
+                    diskon = j * (20/100)
+                    print("Selamat! Anda mendapatkan potongan harga sebesar 20%")
             else:
-                print("Selamat! Anda mendapatkan potongan harga sebesar 10%")
-        elif level == "Gold":
-            if i < 5:
-                print("Selamat! Anda mendapatkan potongan harga sebesar 10%")
-            else:
-                print("Selamat! Anda mendapatkan potongan harga sebesar 15%")
-        elif level == "Diamond":
-            if i < 5:
-                print("Selamat! Anda mendapatkan potongan harga sebesar 15%")
-            else:
-                print("Selamat! Anda mendapatkan potongan harga sebesar 20%")
-else:
-    print("Total harga produk: ", j)
-# diskon = 
+                print("Total harga produk: ", j)
+            setelah_diskon = j - diskon 
+            print('diskonya yaitu : {}'.format(int(diskon)))
+            print("Harga setelah diskon : {}".format(int(setelah_diskon)))
+print("Terimakasih sudah belanja di Toko Kami!")
+
+
+# total = 50000
+# setelah_diskon = total
+ 
+# if total < 100000:
+#     diskon = total * (5/100)
+# else:
+#     diskon = total * (10/100)
+ 
+# setelah_diskon = total - diskon
+# print('diskonya yaitu : {}'.format(int(diskon)))
+# print('Harga setelah diskon : {}'.format(int(setelah_diskon)))
+
+
 # total_harga = j - (diskon/100 * j)
 
+# import sys
+# import msvcrt
 
-                
-
-
-print("Terimakasih sudah belanja di Toko Kami!")
+# passwor = ''
+# while True:
+#     x = msvcrt.getch()
+#     if x == '\r':
+#         break
+#     sys.stdout.write('*')
+#     passwor +=x
+# print('\n'+passwor)
